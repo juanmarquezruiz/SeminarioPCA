a660384ff4946c3b33df43f1662753b676ef1fa3
Juan Marquez <Juanmarquezr@gmail.com>
2023-01-12 21:24:49 -0500

    Clase dia 12

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,21 +1,22 @@
-import { Component, ViewChild } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { images64 } from 'src/assets/images64';
+ 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-})
+}) 
+
 export class HomePage {
 
+  public JpgBase64 = images64;
+  
   constructor() {}
 
-  @ViewChild('popover')  popover!: { event: Event; };
-
-  isOpen = false;
-  
-presentPopover(e: Event) {
-  this.popover.event = e;
-  this.isOpen = true;
+  slideOpt = {
+    initialSlide: 1, //slide inicial (primero) [0,1,2,3]
+    slidesPerView: 1, //configuramos un slide por vista
+    centerSlides: true, //que las slides enten centradas
+    speed: 400 //velocidad movimiento de los slides
+  }
 }
-}
-
